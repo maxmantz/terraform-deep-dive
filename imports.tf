f8a6fda87c4cba6874501f3b8f44d4d09e294550
Maximilian Mantz <maximilian.mantz@eticor.com>
2025-02-07 21:48:39 +0100

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,48 @@
+# ##################################################################################
+# # IMPORTS
+# ##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-0b235b27910c7741d"
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-00e70598d9b513374" #PublicSubnet1
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-08aecf8ed0f1c006e" #PublicSubnet2
+# }
+
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-01388335d3d047645" #InternetGateway
+# }
+
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-0736586f3d78c68e0_0.0.0.0/0" #DefaultPublicRoute
+# }
+
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-0736586f3d78c68e0" #PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-00e70598d9b513374/rtb-0736586f3d78c68e0" #PublicSubnet1/PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-08aecf8ed0f1c006e/rtb-0736586f3d78c68e0" #PublicSubnet2/PublicRouteTable
+# }
+
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-04c19679adb387daa" #NoIngressSecurityGroup
+# }
